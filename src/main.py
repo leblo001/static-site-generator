5a8827795eb328c743deb35dc969e6a5150b6b54
leblo001 <zacharyleblond@gmail.com>
2025-05-28 17:38:34 -0700

    setting up pages

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
@@ -13,9 +14,13 @@ def main():
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
 
+    basepath = "/"
+    if sys.argv[1]:
+        basepath = sys.argv[1]
+        
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 main()
